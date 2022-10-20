510bf0fbe5765444db54ce71595032167e4f7328
SaltSith66 <SaltSith66@gmail.com>
2022-10-20 23:56:29 +0200

    add app config with addressess and names

---
+++ config.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define zmq_plugin_task_QUEUE_NAME  "/zmq_plugin_sensors_monitor_receiver_queue"
+
+#define PLUGIN_ADDR "tcp://localhost:5558"
+#define PLUGIN_ROLE REQUESTER
+
+#define wakeup_message "Arised\r\n"
