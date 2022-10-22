acffe58bf2c14d7d3a79b0c2ada255de9a81f0df
SaltSith66 <SaltSith66@gmail.com>
2022-10-22 15:52:26 +0200

    postman push message to viewer task, check input messages if is it hello message, then return status to zmq plugin

--- postman/postman.h
+++ postman/postman.h
@@ -3,4 +3,6 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-void postman_send_message(uint8_t *msg, size_t msg_len);
+int postman_send_message(uint8_t *msg_buff, size_t msg_buff_size);
+
+size_t postman_hello_message(uint8_t *proto_buff, const size_t buff_size);
