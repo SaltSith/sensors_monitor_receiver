d38f113e92b0d85767e24455a930c9c9b6992264
SaltSith66 <SaltSith66@gmail.com>
2022-10-22 18:19:49 +0200

    use stderr

--- viewer/viewer.c
+++ viewer/viewer.c
@@ -24,12 +24,11 @@ static void
         int result = select(viewer_queue + 1, &rfds, NULL, NULL, NULL);
 
         if (result == -1) {
-            printf("Error on select.\r\n");
+            fprintf(stderr, "Select error\r\n");
         } else if (result) {
-            printf("Data is available on viewer task queue.\r\n");
             viewer_queue_pop();
         } else {
-            printf("No data within given timeout.\r\n");
+            fprintf(stderr, "No data within given timeout.\r\n");
         }
     }
 
